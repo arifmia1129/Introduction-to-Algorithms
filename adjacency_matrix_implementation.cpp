a68099454a0ff084cbb7a2f1944b67afdd920b08
Md. Arif <arifmia1129@gmail.com>
2025-08-17 19:52:00 +0600

    Adjacency Matrix Implementation

---
+++ adjacency_matrix_implementation.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int n, e;
+
+    cin >> n >> e;
+
+    int adjMatArr[n][n];
+
+    memset(adjMatArr, 0, sizeof(adjMatArr));
+
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++) 
+            if(i == j) 
+                adjMatArr[i][j] = 1;
+
+    while(e--) {
+        int a, b;
+
+        cin >> a >> b;
+
+        adjMatArr[a][b] = 1;
+        adjMatArr[b][a] = 1;
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << adjMatArr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
