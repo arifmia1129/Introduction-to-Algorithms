a654ff282da3a5a2200f865c146c3951876b2c02
Md. Arif <arifmia1129@gmail.com>
2025-08-19 21:47:08 +0600

    Single Source Shortest Distance using BFS

--- single_source_shortest_distance_using_bfs.cpp
+++ single_source_shortest_distance_using_bfs.cpp
@@ -2,29 +2,30 @@
 using namespace std;
 vector<int> adjListArr[1005];
 bool vis[1005];
-
+int level[1005];
 
 void bfs(int src) {
     queue<int> q;
 
     q.push(src);
+
     vis[src] = true;
+    level[src] = 0;
 
     while(!q.empty()) {
         int par = q.front();
-
+        
         q.pop();
 
         for(int ch : adjListArr[par]) {
             if(!vis[ch]) {
-                vis[ch] = true;
                 q.push(ch);
+                vis[ch] = true;
+                level[ch] = level[par] + 1;
             }
         }
-    }    
+    }
 }
-
-
 
 int main () {
     int n, e;
@@ -41,6 +42,7 @@ int main () {
     }
 
     memset(vis, false, sizeof(vis));
+    memset(level, -1, sizeof(level));
 
     int s, d;
 
@@ -52,6 +54,8 @@ int main () {
         cout << "YES" << endl;
     else 
         cout << "NO" << endl;
+
+    cout << level[d] << endl;
     
     return 0;
 }
